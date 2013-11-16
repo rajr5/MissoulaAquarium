8187e6bd6880571e3d509c445fc67c889f2c099d
KJ <kj1984um@gmail.com>
2013-11-16 10:07:01 -0700

    n

--- MissoulaAquarium/MasterFormEmployee.Designer.cs
+++ MissoulaAquarium/MasterFormEmployee.Designer.cs
@@ -63,6 +63,8 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.availToursListBox = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.tabLogin.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.tabPage3.SuspendLayout();
@@ -84,6 +86,7 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.label4);
             this.tabPage2.Controls.Add(this.label1);
             this.tabPage2.Controls.Add(this.cancelTourBtn);
             this.tabPage2.Controls.Add(this.signTourBtn);
@@ -97,7 +100,7 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(1049, 716);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Tours Schedule";
+            this.tabPage2.Text = "Tour Sign-Up";
             this.tabPage2.UseVisualStyleBackColor = true;
             this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
             // 
@@ -236,6 +239,7 @@
             // 
             // tabPage7
             // 
+            this.tabPage7.Controls.Add(this.label5);
             this.tabPage7.Controls.Add(this.label10);
             this.tabPage7.Controls.Add(this.pictureBox1);
             this.tabPage7.Controls.Add(this.label9);
@@ -250,7 +254,7 @@
             this.tabPage7.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage7.Size = new System.Drawing.Size(1049, 716);
             this.tabPage7.TabIndex = 5;
-            this.tabPage7.Text = "View/Sign-up Events";
+            this.tabPage7.Text = "Event Sign-up";
             this.tabPage7.UseVisualStyleBackColor = true;
             // 
             // label10
@@ -318,7 +322,7 @@
             // 
             this.label18.AutoSize = true;
             this.label18.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label18.Location = new System.Drawing.Point(578, 370);
+            this.label18.Location = new System.Drawing.Point(578, 346);
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(185, 26);
             this.label18.TabIndex = 2;
@@ -391,7 +395,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(463, 388);
+            this.label2.Location = new System.Drawing.Point(463, 367);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(175, 26);
             this.label2.TabIndex = 43;
@@ -416,6 +420,26 @@
             this.availToursListBox.Name = "availToursListBox";
             this.availToursListBox.Size = new System.Drawing.Size(511, 144);
             this.availToursListBox.TabIndex = 41;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(293, 413);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(435, 13);
+            this.label4.TabIndex = 48;
+            this.label4.Text = "Number    Tour Name              Guide                      Date                 " +
+    "                         Location";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(408, 395);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(349, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Number    Event Name              Date                       Time            Loca" +
+    "tion";
             // 
             // MasterFormEmployee
             // 
@@ -474,5 +498,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ListBox availToursListBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
